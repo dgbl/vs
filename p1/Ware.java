820416db5f8ca4d72f7d7b761e69eeac83fca051
dgbl <33069564+dgbl@users.noreply.github.com>
2017-10-24 21:47:39 +0200

    Add files via upload

---
+++ p1/Ware.java
@@ -0,0 +1,62 @@
+package udp;
+
+/**
+ *
+ * @author DNS
+ */
+public class Ware {
+
+    private String bezeichnung;
+    private int anzahl;
+    private int abnahme;
+
+    /**
+     * @return the bezeichnung
+     */
+    public String getBezeichnung() {
+        return bezeichnung;
+    }
+
+    /**
+     * @param bezeichnung the bezeichnung to set
+     */
+    public void setBezeichnung(String bezeichnung) {
+        this.bezeichnung = bezeichnung;
+    }
+
+    /**
+     * @return the anzahl
+     */
+    public int getAnzahl() {
+        return anzahl;
+    }
+
+    /**
+     * @param anzahl the anzahl to set
+     */
+    public void setAnzahl(int anzahl) {
+        this.anzahl = anzahl;
+    }
+
+    public void entnahme() {
+        int anzahlNeu = getAnzahl() - 1;
+        setAnzahl(anzahlNeu);
+        try {
+            Thread.sleep(100 * getAbnahme());
+        } catch (InterruptedException ex) {
+        }
+    }
+
+    public void zugabe(int zugabe) {
+        int anzahlNeu = anzahl + zugabe;
+        setAnzahl(anzahlNeu);
+    }
+
+    public int getAbnahme() {
+        return abnahme;
+    }
+
+    public void setAbnahme(int abnahme) {
+        this.abnahme = abnahme;
+    }
+}
